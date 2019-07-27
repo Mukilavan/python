bcf9010e27aeeee3a719b816306020d14db5c40b
Mukilavan <53076357+Mukilavan@users.noreply.github.com>
2019-07-27 10:53:49 +0530

    Update Even.py

--- Even.py
+++ Even.py
@@ -1,9 +1,7 @@
 a,n=input().split()
 a=int(a)
 n=int(n)
-if(a<=10000 and n<=10000):
-	for i in range(a,n):
-		if(i%2==0):
-			print(i,end=" ")
-else:
-	print("no")
+for i in range(a,n):
+	if(i%2==0 and i!=a):
+		print(i,end=" ")
+
